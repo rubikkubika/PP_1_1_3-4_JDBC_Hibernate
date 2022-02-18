98c0c3413dd237b8f82255adcd66792d695f2f92
rubikkubika <artw123@mail.ru>
2022-02-18 19:49:23 +0300

    done

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String url = "jdbc:mysql://localhost:3306/new_schema";
+    private static final String user = "root";
+    private static final String password = "root";
+
+    private static Connection con;
+    private static Statement stmt;
+    private static ResultSet rs;
+
+
+    public static void getConnection() throws SQLException {
+        con = DriverManager.getConnection(url, user, password);
+        stmt = con.createStatement();
+    }
+
+    public static ResultSet getQuery(String query) throws SQLException {
+        Util.getConnection();
+        stmt.execute(query);
+        rs = stmt.getResultSet();
+
+        return rs;
+    }
+
+    public static void closeConnection() throws SQLException {
+        con.close();
+        stmt.close();
+    }
 }
